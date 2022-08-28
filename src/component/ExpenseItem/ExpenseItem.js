2d94a9e6a1a5474e03275f37d1275480db15f42e
agiratech-devanathan <devanathan@agiratech.com>
2022-08-28 12:45:42 +0530

    Added new feature.

--- src/component/ExpenseItem/ExpenseItem.js
+++ src/component/ExpenseItem/ExpenseItem.js
@@ -1,28 +1,28 @@
 import './ExpenseItem.css';
-import React, { useState } from 'react';
+import React from 'react';
  import ExpenseDate from '../ExpenseDate/ExpenseDate';
 import Card from '../Card/card';
 function ExpenseItem(props) {
 //here using state method for updating the value user interface and 
 //it takes like an array it has two arguements one is old value, others update function.
 
-const [title,setTitle]=useState(props.title);
-   const clickHandler=()=>{
-    // And here we don't update a new value like this const title="new value";
-    // Instead of we updated by using state method's updte function like 
-    // Here we using setTile function to update the new title
-    //It act specific single single 
-    setTitle('updated!...')
-    console.log(title)
-   }
+//const [title,setTitle]=useState(props.title);
+//    const clickHandler=()=>{
+//     // And here we don't update a new value like this const title="new value";
+//     // Instead of we updated by using state method's updte function like 
+//     // Here we using setTile function to update the new title
+//     //It act specific single single 
+//     setTitle('updated!...')
+//     console.log(title)
+//    }
     return (
         <Card className="expense-item">
              <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Click Me</button>
+            {/* <button onClick={clickHandler}>Click Me</button> */}
         </Card>
     );
 }
---
+++ src/component/Forms/ExpenseForm/expenseForm.js
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import './expenseForm.css';
+const ExpenseForm = (props) => {
+    //titleHandler
+    const [enteredTitle, setEnteredTitle] = useState('');
+    //amountHandler
+    const [enteredAmount, setEnteredAmount] = useState('');
+    //dateHandler
+    const [enteredDate, setEnteredDate] = useState('');
+
+
+    //Alternative ways of using multiple useState, we make objects
+    // const [userInputs, setUserInputs] = useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: '',
+
+    // })
+
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value)
+        //alternative way of overwrite the input value
+        // setUserInputs({
+        //     //copying the userInputs object  and overwritting 
+        //     ...userInputs,
+        //     enteredTitle: event.target.value
+        // })
+    };
+
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value)
+        //alternative way of overwrite the input value
+        // setUserInputs({
+        //     //copying the userInputs object  and overwritting 
+        //     ...userInputs,
+        //     enteredAmount: event.target.value
+        // })
+    };
+
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value)
+        //alternative way of overwrite the input value
+        // setUserInputs({
+        //     //copying the userInputs object  and overwritting 
+        //     ...userInputs,
+        //     enteredDate: event.target.value
+        // })
+        //one more alternative
+        // setUserInputs((previousState) => {
+        //     return { ...previousState, enteredDate: event.target.value }
+        // })
+
+    };
+
+    //Handling Form Submission
+    const submitHandler = (event) => {
+        event.preventDefault();
+        //make all the form Input data are combined into single data object 
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+        props.onSaveExpenseData(expenseData)
+        //clear the all form inputs
+        setEnteredTitle('')
+        setEnteredAmount('')
+        setEnteredDate('')
+    };
+    return (
+        //Implements forms for add Expenese
+        <form>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label>Title</label>
+                    {/* Here we are going to do capture the input value
+                 so that we are using onChange handler, 
+                 titleChangeHandler is a handler function */}
+
+                    <input
+                        type='text'
+                        value={enteredTitle}
+                        onChange={titleChangeHandler} />
+                </div>
+                <div className="new-expense__control">
+                    <label>Amount</label>
+                    <input
+                        type='number'
+                        min='0.01'
+                        step='0.01'
+                        value={enteredAmount}
+                        onChange={amountChangeHandler} />
+                </div>
+                <div className="new-expense__control">
+                    <label>Date</label>
+                    <input
+                        type='date'
+                        min='2019-01-01'
+                        max='2022-12-31'
+                        value={enteredDate}//two way data binding
+                        onChange={dateChangeHandler} />
+                </div>
+            </div>
+            <div className="new-expense__actions">
+                <button onClick={submitHandler}>Add Expense</button>
+            </div>
+        </form>
+    )
+}
+export default ExpenseForm;
