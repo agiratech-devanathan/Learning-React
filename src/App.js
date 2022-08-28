282cd090a741d80c5a3d8e105962bd3b7005fd9d
agiratech-devanathan <devanathan@agiratech.com>
2022-08-28 15:41:16 +0530

    Added looping for display expenses item based on filterrd specific year.

--- src/App.js
+++ src/App.js
@@ -1,46 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Expenses from "./component/Expenses/expenses";
 import NewExpense from "./component/Forms/NewExpense/newExpense";
-
-
+// initial expense items array
+const INITIAL_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  // to add new expense data into initial expenses array using useState
+  const [expenses, setExpense] = useState(INITIAL_EXPENSES);
+
   // return React.createElement(
   //   'div',{},
   //   React.createElement('h2',{},"Let's get Started!"),
   //   React.createElement(Expenses,{items:expenses})
   //   );
 
-  const addExpenseHandler=(expense)=>{ //here is a function is receive data from child to parent component
-    console.log('app.js')
-    console.log(expense)
+  const addExpenseHandler = (expense) => { //here is a function is receive data from child to parent component 
+    //and updating a new value into old list of expenses useing useState setExpense Method
+    setExpense((previousExpense) => {
+      return [expense, ...previousExpense]
+    });
   }
   return (
-   <div>
-    {/* here recive props form child i named here onAddExpenseData and sends arguements too from NewExpense component */}
-    <NewExpense onAddExpenseData={addExpenseHandler}/> 
-    <Expenses items={expenses}/>
-   </div>
+    <div>
+      {/* here recive props form child i named here onAddExpenseData and sends arguements too from NewExpense component */}
+      <NewExpense onAddExpenseData={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
   );
 }
 
--- src/component/Expenses/expenses.js
+++ src/component/Expenses/expenses.js
@@ -3,33 +3,28 @@ import ExpenseItem from '../ExpenseItem/ExpenseItem';
 import Card from '../Card/card';
 import React, { useState } from 'react';
 import ExpenseFilter from '../ExpenseFilter/expenseFilter';
-function Expenses (props){
-  const [filteredYear, setFilteredYear]=useState('2020');
-  const filterChangedHandler=(selectedYear)=>{
-   setFilteredYear(selectedYear)
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState('2020');
+  const filterChangedHandler = (selectedYear) => {
+    setFilteredYear(selectedYear)
   }
-return(
+  const filteredExpenses=props.items.filter(expense=>{
+    return expense.date.getFullYear().toString()===filteredYear;
+  })
+  return (
     <Card className="expenses">
-      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangedHandler}/>
-    <ExpenseItem
-      title={props.items[0].title}
-      amount={props.items[0].amount}
-      date={props.items[0].date}/>
-    
-    <ExpenseItem
-      title={props.items[1].title}
-      amount={props.items[1].amount}
-      date={props.items[1].date}/>
-    <ExpenseItem
-      title={props.items[2].title}
-      amount={props.items[2].amount}
-      date={props.items[2].date}/>
-    <ExpenseItem
-      title={props.items[3].title}
-      amount={props.items[3].amount}
-      date={props.items[3].date}/>
-  </Card>
-)
+      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangedHandler} />
+      {/* looping the expenseItem using map function to display a list of data dynamically */}
+      {filteredExpenses.map((expense) => (
+        //here Filtering data and display based on filtered specific year
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date} />
+      ))}
+    </Card>
+  )
 }
 
 export default Expenses;
