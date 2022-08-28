b2456f1b1e0120935e45fe35f6ffee26b37af194
agiratech-devanathan <devanathan@agiratech.com>
2022-08-28 15:55:47 +0530

    method-1 to display expense data conditionally.

--- src/component/Expenses/expenses.js
+++ src/component/Expenses/expenses.js
@@ -8,21 +8,32 @@ function Expenses(props) {
   const filterChangedHandler = (selectedYear) => {
     setFilteredYear(selectedYear)
   }
+  //here Filtered the expense data based specific year
   const filteredExpenses=props.items.filter(expense=>{
     return expense.date.getFullYear().toString()===filteredYear;
   })
   return (
     <Card className="expenses">
       <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangedHandler} />
+
       {/* looping the expenseItem using map function to display a list of data dynamically */}
-      {filteredExpenses.map((expense) => (
-        //here Filtering data and display based on filtered specific year
+
+      {/* Alternative way of display expense data conditionally */}
+
+      {filteredExpenses.length===0 && <p>No Expense Found.</p> }
+      {filteredExpenses.length>0 && 
+       filteredExpenses.map((expense) => (
+        //here Filtering data and display based on filtered specific year and
+
+        // showing custom message if expense not found for soecific year
+        //method -1 
         <ExpenseItem
           key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date} />
-      ))}
+      ))}  
+  
     </Card>
   )
 }
